b900ff92f56d5663badbfca1c61f71fffe1aa5df
hisashi1919 <hisanori951.81@gmail.com>
2024-04-20 18:06:38 +0900

    ハンバーガーメニュー途中

--- src/js/script.js
+++ src/js/script.js
@@ -27,4 +27,17 @@ color.on('inview', function(){
  });
 });
 
+  //ナビバートグル
+  $('.js-hamburger').on('click', function () {
+    if ($('.js-hamburger').hasClass('is-open')) {
+      // $('.js-drawer-menu').fadeOut();
+      $('.js-drawer-menu').removeClass('is-open');
+      $(this).removeClass('is-open');
+    } else {
+      // $('.js-drawer-menu').fadeIn();
+      $('.js-drawer-menu').addClass('is-open');
+      $(this).addClass('is-open');
+    }
+  });
+
 });
