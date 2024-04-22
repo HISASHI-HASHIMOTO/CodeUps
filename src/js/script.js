596e6c326bdb3184d6bcaf2715327b29c4dd2740
hisashi1919 <hisanori951.81@gmail.com>
2024-04-23 06:54:27 +0900

    4/23

--- src/js/script.js
+++ src/js/script.js
@@ -30,14 +30,13 @@ color.on('inview', function(){
   //ナビバートグル
   $('.js-hamburger').on('click', function () {
     if ($('.js-hamburger').hasClass('is-open')) {
-      // $('.js-drawer-menu').fadeOut();
-      $('.js-drawer-menu').removeClass('is-open');
+      $('.js-drawer-menu').fadeOut();
       $(this).removeClass('is-open');
     } else {
-      // $('.js-drawer-menu').fadeIn();
-      $('.js-drawer-menu').addClass('is-open');
+      $('.js-drawer-menu').fadeIn();  
       $(this).addClass('is-open');
     }
   });
+
 
 });
